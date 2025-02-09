f0451d417f506a934ea2c8ad4ca514d1b1110ca4
finn-rules <finn.rowles@gmail.com>
2025-02-09 13:29:33 -0600

    Update BlockyGame.java
    Introduced the Fisher-Yates algorithm to sort + randomize an array of all possible piece kinds.
    New additions: 
    the shuffle function, which takes in an array of piece kinds and shuffles it.
    new fields: shuffled: a seven-length array of all possible piece kinds
     index, the index of shuffled, resetting when the array is depleted
    blockygame shuffles when the game is created,
    bugfix: trySpawnBlock now uses indices of "shuffled" as opposed to the I block.

--- src/main/java/com/gamewerks/blocky/engine/BlockyGame.java
+++ src/main/java/com/gamewerks/blocky/engine/BlockyGame.java
@@ -11,20 +11,41 @@ public class BlockyGame {
     private Direction movement;
     
     private int lockCounter;
+    // keep track of piece index
+    private int index = 0;
+    PieceKind[] shuffled = { PieceKind.I, J, L, O, S, T, Z };
     
     public BlockyGame() {
         board = new Board();
         movement = Direction.NONE;
         lockCounter = 0;
+        shuffle(shuffled);
         trySpawnBlock();
     }
     
-private void trySpawnBlock() {
+    private void trySpawnBlock() {
         if (activePiece == null) {
-            activePiece = new Piece(PieceKind.I, new Position(0, Constants.BOARD_WIDTH / 2 - 2));
+            if(index < 7) {
+            activePiece = new Piece(shuffled[index], new Position(0, Constants.BOARD_WIDTH / 2 - 2));
+            } else {
+                shuffle(shuffled);
+                index = 0;
+                activePiece = new Piece(shuffled[index], new Position(0, Constants.BOARD_WIDTH / 2 - 2));
+            }
+            // increment index.
+            index++;
             if (board.collides(activePiece)) {
                 System.exit(0);
             }
+        }
+    }
+    
+    public static void shuffle(PieceKind[] arr) {
+       for (int i = arr.length - 1; i >= 0; i--) {
+            int randomIndex = (int)(Math.random() * arr.length);
+            PieceKind temp = arr[i];
+            arr[i] = arr[randomIndex];
+            arr[randomIndex] = temp; 
         }
     }
     
@@ -73,6 +94,7 @@ private void trySpawnBlock() {
         trySpawnBlock();
         processMovement();
         processGravity();
+        // printWell(board.getWell());
         processClearedLines();
     }
     
@@ -80,6 +102,14 @@ private void trySpawnBlock() {
         return board.getWell();
     }
     
+    public void printWell(boolean[][] arr) {
+        for(int i = 0; i < 22; i++) {
+            for(int j = 0; j < 10; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+        System.out.println();
+        }
+    }
     public Piece getActivePiece() { return activePiece; }
     public void setDirection(Direction movement) { this.movement = movement; }
     public void rotatePiece(boolean dir) { activePiece.rotate(dir); }
